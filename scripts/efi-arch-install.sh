e493a8741b42f81f84f16b6d2d8ff39fec317c1f
Joao M. F. Santos <joaomfsantos@hotmail.com>
2025-08-06 09:35:15 +0200

    Update efi-arch-install.sh

--- scripts/efi-arch-install.sh
+++ scripts/efi-arch-install.sh
@@ -1,56 +1,24 @@
 #!/bin/bash
 set -e
 
-
-# List and ask for the target drive
-echo "Available drives:"
-lsblk -d -o NAME,SIZE,MODEL
-echo
-read -rp "Enter the drive to install Arch Linux on (e.g., /dev/sda): " DRIVE
-
 reflector --threads 10 -c Germany --fastest 3 -p http --sort rate --save /etc/pacman.d/mirrorlist
 
 sed -i '/ParallelDownloads/c\ParallelDownloads = 50' /etc/pacman.conf
-
-ROOT_PASS=joao
-USER_PASS=joao
-USERNAME=joao
-
-# Ask for root password
-# while true; do
-#     read -rsp "Enter new root password: " ROOT_PASS
-#     echo
-#     read -rsp "Confirm root password: " ROOT_PASS2
-#     echo
-#     [[ "$ROOT_PASS" == "$ROOT_PASS2" ]] && break
-#     echo "Passwords do not match. Try again."
-# done
-
-# # Ask for new user info
-# read -rp "Enter new username: " USERNAME
-# while true; do
-#     read -rsp "Enter password for $USERNAME: " USER_PASS
-#     echo
-#     read -rsp "Confirm password for $USERNAME: " USER_PASS2
-#     echo
-#     [[ "$USER_PASS" == "$USER_PASS2" ]] && break
-#     echo "Passwords do not match. Try again."
-# done
-
 # Partition, format and mount
-parted -s $DRIVE mklabel gpt
-parted -s $DRIVE mkpart primary fat32 1MiB 512MiB
-parted -s $DRIVE set 1 esp on
-parted -s $DRIVE mkpart primary btrfs 512MiB 100%
+parted -s /dev/sda mklabel gpt
+parted -s /dev/sdb mklabel gpt
+parted -s /dev/sdb mkpart primary fat32 1MiB 512MiB
+parted -s /dev/sdb set 1 esp on
+parted -s /dev/sdb mkpart primary btrfs 512MiB 100%
+parted -s /dev/sda mkpart primary btrfs 1MiB 100%
 
-EFI_PART="${DRIVE}1"
-ROOT_PART="${DRIVE}2"
+mkfs.fat -F32 /dev/sdb1
+mkfs.btrfs -f /dev/sdb2
+mkfs.btrfs -f /dev/sda1
 
-mkfs.fat -F32 $EFI_PART
-mkfs.btrfs -f $ROOT_PART
-
-mount $ROOT_PART /mnt
-mount --mkdir $EFI_PART /mnt/boot
+mount /dev/sdb2 /mnt
+mount --mkdir /dev/sdb1 /mnt/boot
+mount --mkdir /dev/sda1 /mnt/home
 
 # Install base system
 pacstrap -c /mnt base linux linux-firmware-intel sudo nano grub efibootmgr intel-media-driver intel-gpu-tools \
@@ -59,12 +27,6 @@ ripgrep yazi bash-completion starship zoxide bat fzf man-db man-pages reflector 
 pipewire-jack otf-font-awesome noto-fonts ttf-hack archlinux-wallpaper tlp xdg-user-dirs stress pkgfile stow\
 gst-libav gst-plugin-va gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly \
 hyprland foot wofi waybar hyprlock hypridle hyprpicker mako pavucontrol blueman thunar thunar-volman gvfs ristretto mousepad nwg-drawer nwg-look wmctrl brightnessctl qt5ct qt6ct hyprpolkitagent breeze-icons breeze-gtk
-
-#sway swaybg swaylock swayidle foot blueman brightnessctl pavucontrol waybar autotiling wofi xorg-xwayland
-#qt6ct breeze-icons breeze-gtk kvantum
-#plasma dolphin kio-admin konsole kate ark kwalletmanager partitionmanager
-#lightdm-gtk-greeter i3 dmenu brightnessctl pavucontrol thunar thunar-volman ristretto mousepad autotiling
-#xfce4 xfce4-goodies
 
 sed -i "s/'fallback'//g" /mnt/etc/mkinitcpio.d/linux.preset
 
@@ -87,7 +49,7 @@ echo "root:$ROOT_PASS" | arch-chroot /mnt chpasswd
 
 # Install bootloader
 #arch-chroot /mnt pacman --noconfirm -S grub efibootmgr
-arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=$DRIVE
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Archlinux
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 arch-chroot /mnt sed -i '/^GRUB_CMDLINE_LINUX=/ {s/"$/modprobe.blacklist=nvidia,nvidia_modeset,nvidia_drm,nvidia_uvm,nouveau mitigations=off quiet splash"/;}' /etc/default/grub
 sed -i '/^GRUB_TIMEOUT=5$/c\GRUB_TIMEOUT=0' /mnt/etc/default/grub
@@ -112,14 +74,6 @@ echo "Chaotic AUR repository added to /etc/pacman.conf"
 arch-chroot /mnt systemctl enable NetworkManager tlp bluetooth rtkit-daemon
 arch-chroot /mnt usermod -aG input $USERNAME
 arch-chroot /mnt bash -c "echo '%wheel ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"
-
-# Setup dotfiles
-#cp -v .bashrc /mnt/etc/bash.bashrc
-#cp -v .nanorc /mnt/etc/nanorc
-#cp -v .bashrc /mnt/etc/skel/
-#tar xvf dotfiles.tar.gz --directory=/mnt/home/$USERNAME/
-#tar xvf dotfiles.tar.gz --directory=./
-#mv dotfiles/.* /mnt/etc/skel/
 
 # Create new user and set password
 arch-chroot /mnt useradd -m -G wheel -s /bin/bash "$USERNAME"
